eb57f1c1c7e95b415d7f55d410367711c634cd61
joanboix <joanboix@iesebre.com>
2023-05-30 13:50:29 +0200

    video 10

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,   
-            premium: true,         
+            cart: [],
+            premium: true
         }
     },
-    methods: {}
-        
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+    }
 })
