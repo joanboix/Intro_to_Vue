86c5019b7b8f9ad421de5cecce40aa5ff345d783
joanboix <joanboix@iesebre.com>
2023-05-30 14:08:42 +0200

    video 11

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -40,6 +40,8 @@ app.component('product-display', {
           </button>
         </div>
       </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+      <review-form @review-submitted="addReview"></review-form>
     </div>`,
     data() {
       return {
@@ -50,13 +52,17 @@ app.component('product-display', {
           variants: [
             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
             { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-          ]
+          ],
+            reviews: []
       }
     },
     methods: {
         addToCart() {
             this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
         },
+        addReview(review) {
+            this.reviews.push(review)
+        }
     },
     computed: {
         title() {
